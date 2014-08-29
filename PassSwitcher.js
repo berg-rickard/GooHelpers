eade0506a238bf0619ef4888092c457382ae7060
Rickard Berg <rickard@emerbee.se>
2014-08-29 08:24:29 +0200

    Added passswitcher to master

---
+++ PassSwitcher.js
@@ -0,0 +1,45 @@
+'use strict';
+(function(global) {
+	var originalPass = null;
+	var originalComposer = null;
+	var addedPass = null;
+	
+	var PassSwitcher = {
+		switchPass: function(pass, ctx, goo) {
+			var addedPass = pass;
+			var renderSystem = ctx.world.getSystem('RenderSystem');
+			// Get or create composer
+			var composer
+			if (renderSystem.composers.length) {
+				composer = renderSystem.composers[0];
+				originalPass = composer.passes.shift();
+				pass.renderToScreen = false;
+			} else {
+				originalComposer = composer = new goo.Composer();
+				renderSystem.composers.push(composer);
+				pass.renderToScreen = true;
+			}
+			// Add the post effect
+			composer.passes.unshift(pass);
+			if (composer.size) {
+				if (pass.updateSize instanceof Function) {
+					pass.updateSize(composer.size);
+				}
+				pass.viewportSize = composer.size;
+			}
+		},
+		switchBack: function(ctx, goo) {
+			var renderSystem = ctx.world.getSystem('RenderSystem');
+			if (originalComposer) {
+				// If we created a post effect chain, remove it
+				goo.ArrayUtil.remove(renderSystem.composers, originalComposer);
+			} else {
+				// Otherwise, remove the post effect and put back the outpass
+				var composer = renderSystem.composers[0];
+				goo.ArrayUtil.remove(composer.passes, addedPass);
+				composer.passes.unshift(originalPass);
+			}
+		}
+	};
+	global.PassSwitcher = PassSwitcher;
+}(window));
